98168b27b898192c303b83d4497b8bbf9f312128
ModouLaminJagne <89850614+ModouLaminJagne@users.noreply.github.com>
2022-12-20 14:34:43 +0000

    created the expenditure function.

---
+++ financialtracker/src/Expenditure/Expenditure.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddItem from '../AddItem';
+
+const Expenditure = () => {
+  return (
+    <div>
+      <AddItem />
+    </div>
+  );
+};
+
+export default Expenditure;
